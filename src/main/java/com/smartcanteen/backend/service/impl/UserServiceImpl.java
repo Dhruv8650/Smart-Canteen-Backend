736efe560c3eabed2f990b7798731866c25618ce
Dhruv8650 <dhruvsingh19k@gmail.com>
2026-02-19 14:44:31 +0530

    Fixed login after enabling BCrypt hashing

--- src/main/java/com/smartcanteen/backend/service/impl/UserServiceImpl.java
+++ src/main/java/com/smartcanteen/backend/service/impl/UserServiceImpl.java
@@ -3,21 +3,28 @@ package com.smartcanteen.backend.service.impl;
 import com.smartcanteen.backend.entity.User;
 import com.smartcanteen.backend.repository.UserRepository;
 import com.smartcanteen.backend.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository){
+    public UserServiceImpl(UserRepository userRepository,PasswordEncoder passwordEncoder){
         this.userRepository=userRepository;
+        this.passwordEncoder=passwordEncoder;
+
     }
+
     @Override
     public User registerUser(User user){
 
         if(userRepository.findByEmail(user.getEmail()).isPresent()){
-            throw new RuntimeException("Email already exixt");
+            throw new RuntimeException("Email already exist");
         }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         return userRepository.save(user);
     }
 
@@ -25,7 +32,7 @@ public class UserServiceImpl implements UserService {
     public User login(String email, String password) {
         User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException(("User not found")));
 
-        if(!user.getPassword().equals(password)){
+        if(!passwordEncoder.matches(password,user.getPassword())){
             throw new RuntimeException(("Invalid password"));
         }
         return user;
