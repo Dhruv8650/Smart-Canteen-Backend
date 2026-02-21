03c931bf2e28629adfb6c20b9b26f4c160adddca
Dhruv8650 <dhruvsingh19k@gmail.com>
2026-02-21 14:53:36 +0530

    feat(admin): secure admin bootstrap using environment variables

--- src/main/java/com/smartcanteen/backend/BackendApplication.java
+++ src/main/java/com/smartcanteen/backend/BackendApplication.java
@@ -1,13 +1,17 @@
 package com.smartcanteen.backend;
 
+import com.smartcanteen.backend.config.AdminProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AdminProperties.class)
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+
+        SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/smartcanteen/backend/config/DataLoader.java
@@ -0,0 +1,35 @@
+package com.smartcanteen.backend.config;
+
+import com.smartcanteen.backend.entity.Role;
+import com.smartcanteen.backend.entity.User;
+import com.smartcanteen.backend.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class DataLoader {
+
+    @Bean
+    CommandLineRunner loadAdmin(UserRepository userRepository,
+                                PasswordEncoder passwordEncoder,
+                                AdminProperties adminProperties){
+        return args -> {
+            if(userRepository.findByEmail(adminProperties.getEmail()).isEmpty()){
+                User admin = new User();
+                admin.setName((adminProperties.getName()));
+                admin.setEmail(adminProperties.getEmail());
+                admin.setPassword(passwordEncoder.encode(adminProperties.getPassword()));
+                admin.setRole(Role.ADMIN);
+
+                userRepository.save(admin);
+
+                System.out.println("Default ADMIN created successfully.");
+            }
+            else{
+                System.out.println("ADMIN already exists,");
+            }
+        };
+    }
+}
