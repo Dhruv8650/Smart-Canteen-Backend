2200f77842ca18bf33c795c0eda3508cc846e607
Dhruv8650 <dhruvsingh19k@gmail.com>
2026-03-04 19:08:58 +0530

    feat(cart): add secured cart controller with JWT-based user extraction

---
+++ src/main/java/com/smartcanteen/backend/controller/CartController.java
@@ -0,0 +1,36 @@
+package com.smartcanteen.backend.controller;
+
+import com.smartcanteen.backend.dto.request.AddToCartRequestDTO;
+import com.smartcanteen.backend.entity.User;
+import com.smartcanteen.backend.exception.UserNotFoundException;
+import com.smartcanteen.backend.repository.UserRepository;
+import com.smartcanteen.backend.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.PublicKey;
+
+@RestController
+@RequestMapping("/cart")
+@RequiredArgsConstructor
+public class CartController {
+    private final CartService cartService;
+    private final UserRepository userRepository;
+
+    @PostMapping("/add")
+    @PreAuthorize("hasRole('USER')")
+    public String addToCart(@RequestBody AddToCartRequestDTO request,
+                            @AuthenticationPrincipal UserDetails userDetails){
+        User user=userRepository.findByEmail(userDetails.getUsername())
+                .orElseThrow(()-> new UserNotFoundException("User not found"));
+        cartService.addToCart(request,user);
+
+        return "Item added to cart successfully";
+    }
+}
