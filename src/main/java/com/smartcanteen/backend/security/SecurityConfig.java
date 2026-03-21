cf7801b78cb209a7ab2d69f8d5c3878c10497ee6
Dhruv8650 <dhruvsingh19k@gmail.com>
2026-03-21 21:59:42 +0530

    fix(Food): allow public access to menu endpoint

--- src/main/java/com/smartcanteen/backend/security/SecurityConfig.java
+++ src/main/java/com/smartcanteen/backend/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.smartcanteen.backend.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -54,6 +55,8 @@ public class SecurityConfig {
                                 "/ws-orders/**",
                                 "/test/**"
                         ).permitAll()
+
+                        .requestMatchers(HttpMethod.GET,"/menu").permitAll()
 
                         // Health Endpoints
                         .requestMatchers("/", "/auth/**").permitAll()
