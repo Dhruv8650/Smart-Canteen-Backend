c8ef730437311dd3e4132962bd5c719992499661
Dhruv8650 <dhruvsingh19k@gmail.com>
2026-03-28 01:00:59 +0530

    fix(Redis): resolve env variable injection issue

--- src/main/java/com/smartcanteen/backend/config/RedisConfig.java
+++ src/main/java/com/smartcanteen/backend/config/RedisConfig.java
@@ -1,7 +1,10 @@
 package com.smartcanteen.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -9,9 +12,29 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        config.setPassword(password);
+
+        LettuceClientConfiguration clientConfig =
+                LettuceClientConfiguration.builder()
+                        .useSsl()
+                        .build();
+
+        return new LettuceConnectionFactory(config, clientConfig);
     }
 
     @Bean
@@ -23,7 +46,6 @@ public class RedisConfig {
         template.setValueSerializer(new StringRedisSerializer());
 
         template.afterPropertiesSet();
-
         return template;
     }
 }
